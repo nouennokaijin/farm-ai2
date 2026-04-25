4d0475fc3ace84816c6b8aa791a471f6b33b98dc
nouennokajin <nouennokajin@example.com>
2026-04-25 23:56:42 +0900

    強制テスト

--- functions/index.js
+++ functions/index.js
@@ -55,3 +55,8 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log("Server running on port " + PORT);
 });
+
+//
+const { createPage } = require("./utils/notion");
+
+createPage("強制テスト", "これは強制本文テスト");
