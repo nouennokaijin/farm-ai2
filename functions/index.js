bf99160cb773e5af2f2cf0d2263481bb7dcc19ea
nouennokajin <nouennokajin@example.com>
2026-04-26 17:11:17 +0900

    fix stop deproy

--- functions/index.js
+++ functions/index.js
@@ -30,9 +30,5 @@ app.post("/webhook", async (req, res) => {
   }
 });
 
-//app.listen(process.env.PORT || 3000);
-const PORT = process.env.PORT || 3000;
+app.listen(process.env.PORT || 3000);
 
-app.listen(PORT, () => {
-  console.log(`🚀 Server running on port ${PORT}`);
-});
