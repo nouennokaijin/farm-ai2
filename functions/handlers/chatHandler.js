dec7b91152524fd3a62dd7cae2bb11c042d46d5c
nouennokajin <nouennokajin@example.com>
2026-05-02 16:06:13 +0900

    update

--- functions/handlers/chatHandler.js
+++ functions/handlers/chatHandler.js
@@ -1,5 +1,9 @@
 // chatHandler
+const Groq = require("groq-sdk");
 
+const client = new Groq({
+  apiKey: process.env.GROQ_API_KEY,
+});
 
 async function handleChat(event) {
   const replyToken = event.replyToken;
